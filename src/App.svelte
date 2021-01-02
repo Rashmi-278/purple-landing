f653386793ca9279b399d10b4e4ffe87445dfeac
Rashmi <rashmiabbigeri@gmail.com>
2021-01-03 00:08:03 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<!-- App.svelte -->
+<script>
+    import { Router, Route } from "svelte-routing";
+  
+    
+  
+    // No Layout Pages
+    import Index from "./views/Index.svelte";
+  
+    export let url = "";
+  </script>
+  
+  <Router url="{url}">
+     <Route path="/" component="{Index}" />
+  </Router>
